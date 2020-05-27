0db70f2f85742e032f6a6b17f0d17bdfd567f141
thatsdrippe <drunnel04@gmail.com>
2020-05-27 00:47:30 +0000

    Making Hello World

---
+++ MyClassName.java
@@ -0,0 +1,8 @@
+package com.company;
+import java.lang.String;
+import java.lang.System;
+public class MyClassName {
+    public static void main(String[]args){
+        System.out.println("Hello World");
+    }
+}
